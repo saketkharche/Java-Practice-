98c2484cd406beaa6ebe39550e46367f68e0dcbe
saketkharche <kharchesaket@gmail.com>
2023-09-27 08:40:31 +0530

    Java Practice

--- Q3.java
+++ Q3.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("ENter cost of three item");
-        float item1=sc.nextFloat();
-        float item2=sc.nextFloat();
-        float item3=sc.nextFloat();
+        float item1 = sc.nextFloat();
+        float item2 = sc.nextFloat();
+        float item3 = sc.nextFloat();
         float total;
         System.out.println("Cost of all item are:");
-        System.out.println(total=item1+item2+item3);
-        float Gst = total+(0.18f*total);
-        System.out.println(After Gst);
+        System.out.println(total = item1 + item2 + item3);
+        float Gst = total + (0.18f * total);
+        System.out.println("After Gst");
         System.out.println(Gst);
 
     }
