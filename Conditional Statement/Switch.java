700546a22e084382bf6d04a7150cbff1c69df7c6
saketkharche <kharchesaket@gmail.com>
2023-09-29 09:58:29 +0000

    Sep 29, 2023, 3:28 PM

--- Conditional Statement/Switch.java
+++ Conditional Statement/Switch.java
@@ -1,5 +1,16 @@
 public class Switch {
     public static void main(String[] args) {
-        
+        int number = 2;
+        switch (number) {
+            case 1:
+                System.out.println("samosa");
+            case 2:
+                System.out.println("burger");
+            case 3:
+                System.out.println("mango shake");
+            default:
+                System.out.println("wake up to reality");
+
+        }
     }
 }
