5c5990677d83386705ead7fc28b4e378580152d9
saketkharche <kharchesaket@gmail.com>
2023-09-29 11:15:25 +0000

    Sep 29, 2023, 4:45 PM

--- Conditional Statement/Question/Q1.java
+++ Conditional Statement/Question/Q1.java
@@ -1,7 +1,15 @@
-package Question;
+import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number :");
+        int A = sc.nextInt();
+        if (A > 0) {
+            System.out.println("the number is positive:" + A);
+        } else {
+            System.out.println("number is negative:" + A);
+        }
 
     }
 }
