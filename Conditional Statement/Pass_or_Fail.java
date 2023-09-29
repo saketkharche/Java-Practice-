6b448c339b3a8a9ac490214f3c6238dbe0bc2c2f
saketkharche <kharchesaket@gmail.com>
2023-09-29 09:41:43 +0000

    Sep 29, 2023, 3:11 PM

--- Conditional Statement/Pass_or_Fail.java
+++ Conditional Statement/Pass_or_Fail.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Pass_or_Fail {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+        String reportCard = marks >= 33 ? "PASS" : "FAIL";
+        System.out.println(reportCard);
     }
 }
