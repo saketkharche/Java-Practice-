f8dcbf01b14b208098773c64b6897c41c6d373ac
saketkharche <kharchesaket@gmail.com>
2023-09-29 12:33:50 +0530

    java

---
+++ Conditional Statement/ifelse.java
@@ -0,0 +1,16 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int age = 22;
+        if(age >= 18){
+            System.out.println("You can vote");
+        } 
+        if(age>13 && age<18){
+            System.out.println("tennagre");
+
+        }
+        else {
+            System.out.println("You cannot vote");
+        }
+
+	}
+}
