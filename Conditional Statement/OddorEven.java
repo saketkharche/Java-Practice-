96cfb452adc72cf6c488c049255d20b1a811ef1e
saketkharche <kharchesaket@gmail.com>
2023-09-29 07:40:58 +0000

    Sep 29, 2023, 1:10 PM

--- Conditional Statement/OddorEven.java
+++ Conditional Statement/OddorEven.java
@@ -4,10 +4,10 @@ public class OddorEven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        if(number % 2 ==0){
+        if (number % 2 == 0) {
             System.out.println("even");
-        }else{
+        } else {
             System.out.println("odd");
         }
-    }    
+    }
 }
