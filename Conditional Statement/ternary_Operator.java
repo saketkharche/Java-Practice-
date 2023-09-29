0b3e8edc630d55a804e76684dd599f294b1166ef
saketkharche <kharchesaket@gmail.com>
2023-09-29 09:36:41 +0000

    Sep 29, 2023, 3:06 PM

--- Conditional Statement/ternary_Operator.java
+++ Conditional Statement/ternary_Operator.java
@@ -1,5 +1,8 @@
 public class ternary_Operator {
     public static void main(String[] args) {
-        
+        int no =4 ;
+        String type = ((no%2) ==0)?"even":"odd";
+        System.out.println(type);
     }
+
 }
