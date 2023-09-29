4486ef6c488f40602a8df11dfb3761b7b587c8d6
saketkharche <kharchesaket@gmail.com>
2023-09-29 12:57:25 +0530

    java

---
+++ Conditional Statement/Largestof3.java
@@ -0,0 +1,17 @@
+
+
+public class Largestof3 {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 3;
+        int C = 6;
+
+        if((A>=B)&&(A>=C)){ 
+            System.out.println("A");
+        }else if(B>=C){
+            System.out.println("B");
+        }else{
+            System.out.println("C");
+        }
+    }
+}
---
+++ Conditional Statement/largervalue.java
@@ -0,0 +1,11 @@
+public class largervalue {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        if (A >= B) {
+            System.out.println("A is larger of 2");
+        } else {
+            System.out.println("B is largest of 2");
+        }
+    }
+}
