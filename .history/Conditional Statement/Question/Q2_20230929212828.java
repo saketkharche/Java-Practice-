fba48accc3c4bdfd1c7b023c62774cd10a8ba1bb
saketkharche <kharchesaket@gmail.com>
2023-09-29 21:29:22 +0530

    commit

--- .history/Conditional Statement/Question/Q2_20230929212828.java
+++ .history/Conditional Statement/Question/Q2_20230929212828.java
@@ -3,7 +3,7 @@ public class Q2 {
     public static void main(String[] args) {
         double temp = 103.5;
         if (temp >= 100) {
-            System.out.println("you have fever");
+            System.out.println("ypu have fever");
 
         } else {
             System.out.println("You don't have a fever");
--- Conditional Statement/Question/tempCodeRunnerFile.java
+++ Conditional Statement/Question/tempCodeRunnerFile.java
@@ -1,11 +1,12 @@
+package Question;
 
 public class Q2 {
     public static void main(String[] args) {
         double temp = 103.5;
-        if (temp >= 100) {
-            System.out.println("you have fever");
-
-        } else {
+        if (temp >=100) {
+            System.out.println("ypu have fever");
+            
+        }else{
             System.out.println("You don't have a fever");
         }
     }
