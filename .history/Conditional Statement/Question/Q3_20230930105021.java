7b05a2a65a5a05decad86afd16db318619bd046f
saketkharche <kharchesaket@gmail.com>
2023-09-30 05:22:37 +0000

    Sep 30, 2023, 10:52 AM

--- .history/Conditional Statement/Question/Q3_20230930105021.java
+++ .history/Conditional Statement/Question/Q3_20230930105021.java
@@ -5,33 +5,33 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the week number(1-7)");
         int X = sc.nextInt();
-        switch (X) {
+        switch(X){
             case 1:
-                System.out.println("Monday");
+                    System.out.println("Monday");
                 break;
 
             case 2:
-                System.out.println("Thuesday");
+                    System.out.println("Thuesday");
                 break;
 
             case 3:
-                System.out.println("Wednesday");
+                    System.out.println("Wednesday");
                 break;
-
+            
             case 4:
-                System.out.println("Thursday");
+                    System.out.println("Thuersday");
                 break;
 
             case 5:
-                System.out.println("Friday");
+                    System.out.println("Monday");
                 break;
 
             case 6:
-                System.out.println("Satuday");
+                    System.out.println("Monday");
                 break;
-
+        
             case 7:
-                System.out.println("Sunday");
+                    System.out.println("Monday");
                 break;
 
             default:
