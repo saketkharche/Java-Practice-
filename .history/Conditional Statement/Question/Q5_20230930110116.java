a154bff78bec5e549822e13abcdbe622ef0b8bc1
saketkharche <kharchesaket@gmail.com>
2023-09-30 05:37:10 +0000

    Sep 30, 2023, 11:07 AM

---
+++ .history/Conditional Statement/Question/Q5_20230930110116.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year : ");
+        int year = sc.nextInt();
+        if (year/) {
+            
+        } else {
+            
+        }
+    }
+    
+}
---
+++ .history/Conditional Statement/Question/Q5_20230930110330.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year : ");
+        int year = sc.nextInt();
+        boolean x = (year % 4) ==0;
+        boolean y = (year % 100)!=0
+    }
+    
+}
